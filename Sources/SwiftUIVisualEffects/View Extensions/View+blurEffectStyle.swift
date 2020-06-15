56dfc931b8c41ca893ec0e0dec6d31eec46b204c
Lucas Brown <lucasbrown@lucasbrownish.com>
2020-06-15 13:33:38 -0500

    Add code samples to documentation

--- Sources/SwiftUIVisualEffects/View Extensions/View+blurEffectStyle.swift
+++ Sources/SwiftUIVisualEffects/View Extensions/View+blurEffectStyle.swift
@@ -6,7 +6,21 @@
 import SwiftUI
 
 public extension View {
-	/// Sets the style for blur effects within this view.
+	/**
+	Sets the style for blur effects within this view.
+	
+	To set a specific style for all blur effects and vibrancy effects containing blur effects within a view, use the blurEffectStyle(_:) modifier:
+	```
+	ZStack {
+		backgroundContent
+			.blurEffect()
+		
+		foregroundContent
+			.vibrancyEffect()
+	}
+	.blurEffectStyle(.systemMaterial)
+	```
+	*/
 	func blurEffectStyle(_ style: UIBlurEffect.Style) -> some View {
 		environment(\.blurEffectStyle, style)
 	}
--- Sources/SwiftUIVisualEffects/View Extensions/View+vibrancyEffectStyle.swift
+++ Sources/SwiftUIVisualEffects/View Extensions/View+vibrancyEffectStyle.swift
@@ -6,7 +6,26 @@
 import SwiftUI
 
 public extension View {
-	/// Sets the style for vibrancy effects within this view.
+	/**
+	Sets the style for vibrancy effects within this view.
+	
+	To set a specific style for all vibrancy effects within a view, use the vibrancyEffectStyle(_:) modifier:
+	```
+	ZStack {
+		backgroundContent
+			.blurEffect()
+		
+		VStack {
+			topContent
+				.vibrancyEffect()
+			
+			bottomContent
+				.vibrancyEffect()
+		}
+		.vibrancyEffectStyle(.fill)
+	}
+	```
+	*/
 	func vibrancyEffectStyle(_ style: UIVibrancyEffectStyle) -> some View {
 		environment(\.vibrancyEffectStyle, style)
 	}
